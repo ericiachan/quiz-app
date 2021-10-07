2a54a783d0c54b7d16e0d3454f5cef3d547c8623
Ericia <ericia.crj@gmail.com>
2021-10-07 09:24:40 +0800

    initial commit

---
+++ questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "What does ETF stand for?",
+    answer: "Exchange-Traded Fund",
+    options: [
+      "Empty Trader Fund",
+      "Entry Trading Fund",
+      "Exchange-Traded Fund",
+      "Explicitly Traded Fund"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How are ETFs different from mutual funds?",
+    answer: "ETFs are passively managed while mutual funds are actively managed",
+    options: [
+      "ETFs are passively managed while mutual funds are actively managed",
+      "There is no difference",
+      "Mutual funds are superior in returns",
+      "ETFs are superior in returns"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is one advantage of ETFs?",
+    answer: "Diversification",
+    options: [
+      "High risk, high reward",
+      "Diversification",
+      "There is no advantage",
+      "High yield"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is a feature of ETFs?",
+    answer: "ETFs provide the option of investing in a variety of asset classes",
+    options: [
+      "ETFs provide the option of investing in a variety of asset classes",
+      "ETFs are illiquid",
+      "ETFs do not offer dividends",
+      "ETFs are high risk"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is one difference between ETFs and stocks?",
+    answer: "ETFs are not bound to a single company, whereas stocks are",
+    options: [
+      "ETFs are not traded on stock exchanges, whereas stocks are",
+      "ETFs are not bound to a single company, whereas stocks are",
+      "Both are exactly the same",
+      "ETFs are not widely available, whereas stocks are"
+    ]
+  },
+
+];
